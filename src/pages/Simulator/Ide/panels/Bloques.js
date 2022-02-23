510fddfd24309006bab9262a280da85eca7f3a3c
José Astorga <jose.n.astorga.tobar@gmail.com>
2022-02-23 18:41:04 -0300

    Add blockly dark theme

--- src/pages/Simulator/Ide/panels/Bloques.js
+++ src/pages/Simulator/Ide/panels/Bloques.js
@@ -1,47 +1,62 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Grid from "@mui/material/Grid";
 
-
-import "./Blockly.css"
-import "blockly/python";
-
 import Blockly from "blockly";
-import { BlocklyWorkspace } from "react-blockly";
+import "./Blockly.css";
+import "blockly/python";
+import DarkTheme from "@blockly/theme-dark";
+import { useBlocklyWorkspace } from "react-blockly";
+
 import toolbox from "../../../../michiblock/toolbox";
 
-const PanelBloques = React.memo(({editorRef}) => {
-  const handleOnXmlChange = React.useCallback((xml) => {
-    localStorage.setItem("xml", xml)
-  }, []);
+const PanelBloques = React.memo(({ editorRef }) => {
+  const blocklyRef = useRef(null);
 
-  const handleOnWorkspaceDidChange = React.useCallback((workspace) => {
-    const code = Blockly.JavaScript.workspaceToCode(workspace);
-    if (editorRef.current) editorRef.current.setValue(code);
-  }, [editorRef]);
+  const handleOnWorkspaceDidChange = React.useCallback(
+    (workspace) => {
+      console.log("workspace change");
+      const code = Blockly.Python.workspaceToCode(workspace);
+      if (editorRef.current) editorRef.current.setValue(code);
+    },
+    [editorRef]
+  );
+
+  const { xml } = useBlocklyWorkspace({
+    ref: blocklyRef,
+    toolboxConfiguration: toolbox, // this must be a JSON toolbox definition
+    initialXml: localStorage.getItem("xml"),
+    onWorkspaceChange: handleOnWorkspaceDidChange,
+    workspaceConfiguration: {
+      grid: {
+        spacing: 50,
+        length: 5,
+        colour: "#ccc",
+        snap: true,
+      },
+      move: {
+        scrollbars: {
+          horizontal: false,
+          vertical: true,
+        },
+        wheel: true,
+      },
+      ...(localStorage.getItem("theme") === "dark" && { theme: DarkTheme }),
+    },
+  });
+
+  useEffect(() => {
+    localStorage.setItem("xml", xml);
+  }, [xml]);
 
   return (
-    <Grid className="Blockly"  display="flex" flex={1} width={"100%"} height={"100%"}>
-      <BlocklyWorkspace
-        toolboxConfiguration={toolbox}
-        workspaceConfiguration={{
-          grid: {
-            spacing: 50,
-            length: 5,
-            colour: "#ccc",
-            snap: true,
-          },
-          move: {
-            scrollbars: {
-              horizontal: false,
-              vertical: true,
-            },
-            wheel: true,
-          },
-        }}
-        initialXml={localStorage.getItem("xml")}
-        onXmlChange={handleOnXmlChange}
-        onWorkspaceChange={handleOnWorkspaceDidChange}
-      />
+    <Grid
+      className="Blockly"
+      display="flex"
+      flex={1}
+      width={"100%"}
+      height={"100%"}
+    >
+      <div id="blocklyDiv" ref={blocklyRef} />
     </Grid>
   );
 });
